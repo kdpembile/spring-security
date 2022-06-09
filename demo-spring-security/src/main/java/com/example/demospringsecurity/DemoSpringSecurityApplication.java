dcd7d6a7050c1a038524840e305cb086e6210aa5
kent.david.embile <kent.david.embile@accenture.com>
2022-06-09 21:31:15 +0800

    add bean for model mapper and password encoder

--- demo-spring-security/src/main/java/com/example/demospringsecurity/DemoSpringSecurityApplication.java
+++ demo-spring-security/src/main/java/com/example/demospringsecurity/DemoSpringSecurityApplication.java
@@ -1,10 +1,24 @@
 package com.example.demospringsecurity;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringSecurityApplication.class, args);
