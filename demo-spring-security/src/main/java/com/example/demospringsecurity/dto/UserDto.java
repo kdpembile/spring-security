3bdb4b9031e7c456426d5575d1ee43c5e106aaaf
kent.david.embile <kent.david.embile@accenture.com>
2022-06-10 20:50:54 +0800

    update user dto

--- demo-spring-security/src/main/java/com/example/demospringsecurity/dto/UserDto.java
+++ demo-spring-security/src/main/java/com/example/demospringsecurity/dto/UserDto.java
@@ -1,18 +1,27 @@
 package com.example.demospringsecurity.dto;
 
+import com.github.dozermapper.core.Mapping;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
 
+    @Mapping("username")
     private String username;
 
+    @Mapping("password")
     private String password;
 
+    @Mapping("enabled")
     private boolean enabled;
+
+    @Mapping("authority")
+    private Set<AuthorityDto> authority = new HashSet<>();
 }
