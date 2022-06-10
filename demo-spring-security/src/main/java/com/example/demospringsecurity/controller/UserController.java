4f83f258bd1a3e8a64687cf5f4c2a62b3407b270
kent.david.embile <kent.david.embile@accenture.com>
2022-06-10 19:41:06 +0800

    add endpoint for crud operation

--- demo-spring-security/src/main/java/com/example/demospringsecurity/controller/UserController.java
+++ demo-spring-security/src/main/java/com/example/demospringsecurity/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.example.demospringsecurity.controller;
 
 import com.example.demospringsecurity.dto.UserDto;
+import com.example.demospringsecurity.model.MessageResponse;
 import com.example.demospringsecurity.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @RestController
 public class UserController {
 
@@ -18,5 +29,94 @@ public class UserController {
     @GetMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<UserDto> getUsers(@RequestParam int page, @RequestParam int size) {
         return userService.getUsers(page, size);
+    }
+
+    @GetMapping(path = "/users/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDto getUser(@PathVariable String username) {
+        return userService.getUser(username);
+    }
+
+    @PostMapping(path = "/user/authority/{username}/authority", consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MessageResponse> addAuthorityToUser(@PathVariable String username, @PathVariable String authority) {
+        try {
+            userService.addAuthorityToUser(username, authority);
+
+            MessageResponse response = new MessageResponse();
+            response.setMessage(String.format("Authority of %s was successfully saved to user %s"
+                    , authority, username));
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST
+                    , String.format("Failed to save authority of %s to user %s"
+                    , authority, username), e);
+        }
+    }
+
+    @PostMapping(path = "/user", consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MessageResponse> saveUser(@RequestBody UserDto user) {
+        try {
+            userService.saveUser(user);
+
+            MessageResponse response = new MessageResponse();
+            response.setMessage(String.format("User %s was successfully saved"
+                    , user.getUsername()));
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST
+                    , String.format("Failed to save user %s"
+                    , user.getUsername()), e);
+        }
+    }
+
+    @PutMapping(path = "/user/{username}", consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MessageResponse> updateUser(@PathVariable String username, @RequestBody UserDto user) {
+        try {
+            userService.updateUser(username, user);
+
+            MessageResponse response = new MessageResponse();
+            response.setMessage(String.format("User %s was successfully updated"
+                    , username));
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST
+                    , String.format("Failed to update user %s"
+                    , username), e);
+        }
+    }
+
+    @DeleteMapping(path = "/user/{username}", consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MessageResponse> deleteUser(@PathVariable String username) {
+        try {
+            userService.deleteUser(username);
+
+            MessageResponse response = new MessageResponse();
+            response.setMessage(String.format("User %s was successfully deleted"
+                    , username));
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST
+                    , String.format("Failed to delete user %s"
+                    , username), e);
+        }
     }
 }
