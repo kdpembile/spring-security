48511f06ccd6c7c3cacf473346e20e93897f6081
kent.david.embile <kent.david.embile@accenture.com>
2022-06-21 23:42:00 +0800

    add api info

--- demo-spring-security/src/main/java/com/example/demospringsecurity/config/SpringFoxConfig.java
+++ demo-spring-security/src/main/java/com/example/demospringsecurity/config/SpringFoxConfig.java
@@ -6,13 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
 import java.util.List;
 
 @EnableWebMvc
@@ -27,7 +30,8 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(DemoSpringSecurityApplication.class.getPackageName()))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiDetails());
     }
 
     private ApiKey apiKey() {
@@ -43,5 +47,18 @@ public class SpringFoxConfig {
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
         return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Demo project for Spring Security",
+                "Sample API with Spring Security",
+                "1.0",
+                "Free to use",
+                new Contact("KD", "", "kent.embile@gmail.com"),
+                "",
+                "",
+                Collections.emptyList()
+        );
     }
 }
