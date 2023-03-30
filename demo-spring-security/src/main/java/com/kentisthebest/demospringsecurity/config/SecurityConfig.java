056385a11a9735fd94eb11048441bac377420d9d
kentembutido <kent.david.embile@gmail.com>
2023-03-31 05:07:07 +0800

    move all antMatchers from WebSecurity to HttpSecurity configure methods

--- demo-spring-security/src/main/java/com/kentisthebest/demospringsecurity/config/SecurityConfig.java
+++ demo-spring-security/src/main/java/com/kentisthebest/demospringsecurity/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -40,20 +39,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/login/**").permitAll();
+        http.headers().frameOptions().disable();
 
-        http.authorizeRequests().antMatchers(GET, PATH).hasAnyAuthority(ROLE_USER, ROLE_ADMIN);
-        http.authorizeRequests().antMatchers(POST, PATH).hasAuthority(ROLE_ADMIN);
-        http.authorizeRequests().antMatchers(PUT, PATH).hasAuthority(ROLE_ADMIN);
-        http.authorizeRequests().antMatchers(DELETE, PATH).hasAuthority(ROLE_ADMIN);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests().antMatchers("/login/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/configuration/ui").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/configuration/security").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/webjars/**").permitAll();
+
+        http.authorizeRequests().antMatchers(GET, PATH).hasAnyAuthority(ROLE_USER, ROLE_ADMIN)
+                .antMatchers(POST, PATH).hasAuthority(ROLE_ADMIN)
+                .antMatchers(PUT, PATH).hasAuthority(ROLE_ADMIN)
+                .antMatchers(DELETE, PATH).hasAuthority(ROLE_ADMIN);
 
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(securityKey, authenticationManagerBean()));
@@ -64,15 +72,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui/**",
-                "/webjars/**");
     }
 }
